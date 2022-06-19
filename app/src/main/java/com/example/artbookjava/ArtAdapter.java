27c7dadae7625a20b945df2e57811df85bf69736
DESKTOP-TAKTNKE\resat <onur.kurt@volthread.com>
2022-06-19 19:27:57 +0300

    Initial commit

---
+++ app/src/main/java/com/example/artbookjava/ArtAdapter.java
@@ -0,0 +1,57 @@
+package com.example.artbookjava;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.artbookjava.databinding.RecyclerRowBinding;
+
+import java.util.ArrayList;
+
+public class ArtAdapter extends RecyclerView.Adapter<ArtAdapter.ArtHolder> {
+
+    ArrayList<Art> artArrayList;
+
+    public ArtAdapter(ArrayList<Art> artArrayList) {
+        this.artArrayList = artArrayList;
+    }
+
+    @Override
+    public ArtHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        RecyclerRowBinding binding = RecyclerRowBinding.inflate(LayoutInflater.from(parent.getContext()),parent,false);
+        return new ArtHolder(binding);
+    }
+
+    @Override
+    public void onBindViewHolder(ArtAdapter.ArtHolder holder, int position) {
+        holder.binding.recyclerViewTextView.setText(artArrayList.get(holder.getAdapterPosition()).name);
+        holder.itemView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(holder.itemView.getContext(), ArtActivity.class);
+                intent.putExtra("artId",artArrayList.get(position).id);
+                intent.putExtra("info","old");
+                holder.itemView.getContext().startActivity(intent);
+            }
+        });
+    }
+
+    @Override
+    public int getItemCount() {
+        return artArrayList.size();
+    }
+
+    public class ArtHolder extends RecyclerView.ViewHolder {
+        private RecyclerRowBinding binding;
+
+        public ArtHolder(RecyclerRowBinding binding) {
+            super(binding.getRoot());
+            this.binding = binding;
+        }
+    }
+
+}
